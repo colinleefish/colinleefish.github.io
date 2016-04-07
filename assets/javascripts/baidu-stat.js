246452bb3023b9f52a91812acee259c0d997aae8
Colin Lee <colinleefish@gmail.com>
2016-04-07 22:40:26 +0800

    Added Baidu stat js file.

---
+++ assets/javascripts/baidu-stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?05ede31a1cfb58989dd95b9289c7d592";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
